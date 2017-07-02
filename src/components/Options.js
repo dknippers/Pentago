e56eea2b5fa5a6ac9d21b1a0deb47eaaf0b4926d
Daniël Knippers <dknippers@gmail.com>
2017-07-02 18:39:29 +0200

    Fiene mode weer aangezet

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,22 +1,45 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { getPlayers } from '../selectors/playerSelectors';
+import React from "react";
+import { connect } from "react-redux";
+import { getPlayers } from "../selectors/playerSelectors";
 import {
-  setPlayerName, setPlayerAI, toggleOptions, setAIMoveDelay,
-  setAutomaticRestart, beginTurn, setAnimationsEnabled, setAnimationDuration,
-  setShowLoadSaveButtons, setFieneMode
-} from '../actions';
+  setPlayerName,
+  setPlayerAI,
+  toggleOptions,
+  setAIMoveDelay,
+  setAutomaticRestart,
+  beginTurn,
+  setAnimationsEnabled,
+  setAnimationDuration,
+  setShowLoadSaveButtons,
+  setFieneMode
+} from "../actions";
 
 const Options = ({
-  gameStarted, players, isVisible, aiMoveDelay, automaticRestart, animationsEnabled, animationDuration, fieneMode,
-  setPlayerName, setPlayerAI, toggleOptions, setAIMoveDelay, setAutomaticRestart, setAnimationsEnabled,
-  setAnimationDuration, beginTurn, setShowLoadSaveButtons, showLoadSaveButtons, setFieneMode
+  gameStarted,
+  players,
+  isVisible,
+  aiMoveDelay,
+  automaticRestart,
+  animationsEnabled,
+  animationDuration,
+  fieneMode,
+  setPlayerName,
+  setPlayerAI,
+  toggleOptions,
+  setAIMoveDelay,
+  setAutomaticRestart,
+  setAnimationsEnabled,
+  setAnimationDuration,
+  beginTurn,
+  setShowLoadSaveButtons,
+  showLoadSaveButtons,
+  setFieneMode
 }) => {
   const player1 = players.find(player => player.id === 1);
   const player2 = players.find(player => player.id === 2);
 
   return (
-    <div className={ getClassNames() }>
+    <div className={getClassNames()}>
       <h2>Options</h2>
 
       <div className="option-columns">
@@ -35,50 +58,99 @@ const Options = ({
           <label htmlFor="automaticRestart">Autorestart game</label>
           <label htmlFor="fieneMode">Fiene Mode</label>
 
-          <label htmlFor="showLoadSaveButtons">Show load and save buttons</label>
+          <label htmlFor="showLoadSaveButtons">
+            Show load and save buttons
+          </label>
         </div>
         <div className="option-column">
-          <input type="text" value={ player1.name } id="player-1-name" onChange={ e => setPlayerName(player1.id, e.target.value) } />
-          <input type="checkbox" id="player-1-ai" checked={ player1.isAI } onChange={ e => setPlayerAI(player1.id, e.target.checked) } />
+          <input
+            type="text"
+            value={player1.name}
+            id="player-1-name"
+            onChange={e => setPlayerName(player1.id, e.target.value)}
+          />
+          <input
+            type="checkbox"
+            id="player-1-ai"
+            checked={player1.isAI}
+            onChange={e => setPlayerAI(player1.id, e.target.checked)}
+          />
 
-          <input type="text" value={ player2.name } id="player-2-name" onChange={ e => setPlayerName(player2.id, e.target.value) } />
-          <input type="checkbox" id="player-2-ai" checked={ player2.isAI } onChange={ e => setPlayerAI(player2.id, e.target.checked) } />
+          <input
+            type="text"
+            value={player2.name}
+            id="player-2-name"
+            onChange={e => setPlayerName(player2.id, e.target.value)}
+          />
+          <input
+            type="checkbox"
+            id="player-2-ai"
+            checked={player2.isAI}
+            onChange={e => setPlayerAI(player2.id, e.target.checked)}
+          />
 
-          <input type="text" id="aiMoveDelay" defaultValue={ aiMoveDelay } onBlur={ e => setAIMoveDelay(parseInt(e.target.value, 10)) } />
+          <input
+            type="text"
+            id="aiMoveDelay"
+            defaultValue={aiMoveDelay}
+            onBlur={e => setAIMoveDelay(parseInt(e.target.value, 10))}
+          />
 
-          <input type="checkbox" id="animationsEnabled" checked={ animationsEnabled } onChange={ e => setAnimationsEnabled(e.target.checked) } />
-          <input type="text" id="animationDuration" defaultValue={ animationDuration } onBlur={ e => setAnimationDuration(parseInt(e.target.value, 10)) } />
+          <input
+            type="checkbox"
+            id="animationsEnabled"
+            checked={animationsEnabled}
+            onChange={e => setAnimationsEnabled(e.target.checked)}
+          />
+          <input
+            type="text"
+            id="animationDuration"
+            defaultValue={animationDuration}
+            onBlur={e => setAnimationDuration(parseInt(e.target.value, 10))}
+          />
 
-          <input type="checkbox" id="automaticRestart" checked={ automaticRestart } onChange={ e => setAutomaticRestart(e.target.checked) } />
-          <input type="checkbox" id="fieneMode" checked={ fieneMode } onChange={ e => setFieneMode(e.target.checked) } />
+          <input
+            type="checkbox"
+            id="automaticRestart"
+            checked={automaticRestart}
+            onChange={e => setAutomaticRestart(e.target.checked)}
+          />
+          <input type="checkbox" id="fieneMode" checked={fieneMode} onChange={e => setFieneMode(e.target.checked)} />
 
-          <input type="checkbox" id="showLoadSaveButtons" checked={ showLoadSaveButtons } onChange={ e => setShowLoadSaveButtons(e.target.checked) } />
+          <input
+            type="checkbox"
+            id="showLoadSaveButtons"
+            checked={showLoadSaveButtons}
+            onChange={e => setShowLoadSaveButtons(e.target.checked)}
+          />
         </div>
       </div>
 
-      { isVisible &&
-        gameStarted
-          ? <button type="button" className="btn" onClick={ toggleOptions }>Close</button>
-          : <button type="button" className="btn" onClick={ startGame }>Start game</button>
-      }
+      {isVisible && gameStarted
+        ? <button type="button" className="btn" onClick={toggleOptions}>
+          Close
+          </button>
+        : <button type="button" className="btn" onClick={startGame}>
+          Start game
+          </button>}
     </div>
-  )
+  );
 
   function getClassNames() {
-    const classNames = ['options'];
+    const classNames = ["options"];
 
-    if(isVisible) {
-      classNames.push('visible');
+    if (isVisible) {
+      classNames.push("visible");
     }
 
-    return classNames.join(' ');
+    return classNames.join(" ");
   }
 
   function startGame() {
     toggleOptions();
     beginTurn();
   }
-}
+};
 
 export default connect(
   state => ({
@@ -93,6 +165,15 @@ export default connect(
     fieneMode: state.options.fieneMode
   }),
   {
-    setPlayerName, setPlayerAI, toggleOptions, setAIMoveDelay, setAutomaticRestart,
-    setAnimationsEnabled, setAnimationDuration, beginTurn, setShowLoadSaveButtons, setFieneMode }
+    setPlayerName,
+    setPlayerAI,
+    toggleOptions,
+    setAIMoveDelay,
+    setAutomaticRestart,
+    setAnimationsEnabled,
+    setAnimationDuration,
+    beginTurn,
+    setShowLoadSaveButtons,
+    setFieneMode
+  }
 )(Options);
